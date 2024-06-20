bd06d95978ebcb056cc43f9ac7b872c4f831b9f9
Rudolf Bárdos <bardos@outlook.com>
2024-06-20 14:16:38 +0200

    feat: add dynamodb event source

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,15 +1,17 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { Stream } from 'aws-cdk-lib/aws-kinesis';
 import { StartingPosition } from 'aws-cdk-lib/aws-lambda';
-import { KinesisEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { DynamoEventSource, KinesisEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { myKinesisFunction } from './functions/kinesis-function/resource';
+import { myDynamoDBFunction } from './functions/dynamodb-function/resource';
 
 const backend = defineBackend({
     auth,
     data,
-    myKinesisFunction
+    myKinesisFunction,
+    myDynamoDBFunction
 });
 
 const kinesisStack = backend.createStack('kinesis-stack');
@@ -19,9 +21,15 @@ const kinesisStream = new Stream(kinesisStack, 'KinesisStream', {
     shardCount: 1
 });
 
-const eventSource = new KinesisEventSource(kinesisStream, {
+const kinesisEventSource = new KinesisEventSource(kinesisStream, {
     startingPosition: StartingPosition.LATEST,
     reportBatchItemFailures: true
 });
 
-backend.myKinesisFunction.resources.lambda.addEventSource(eventSource);
+backend.myKinesisFunction.resources.lambda.addEventSource(kinesisEventSource);
+
+const eventSource = new DynamoEventSource(backend.data.resources.tables["Todo"], {
+    startingPosition: StartingPosition.LATEST,
+});
+
+backend.myDynamoDBFunction.resources.lambda.addEventSource(eventSource);
